241c69e91b1e100ace1816a0f4fd8c15c34d118c
Edoardo Lucini <assassineeeto@gmail.com>
2023-03-08 11:30:48 +0100

    set up automated scoring

--- main.py
+++ main.py
@@ -1,54 +1,83 @@
-import argparse
-
 import pandas as pd
-from gensim.models import KeyedVectors
+import multiprocessing as mp
+import os
+import utils
 
-from frameAxis import FrameAxis
+from frameAxis import FrameAxis 
 
+lock = mp.Lock()
+dirname = os.path.dirname(__file__)
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Calculate Moral Foundation Scores Using FrameAxis Approach.')
+# multiprocessing parameters
+NUMBER_OF_PROCESSES = 1
+CHUNK_SIZE = 100000
 
-    parser.add_argument('--input_file',
-                        help='Path to the dataset .csv file containing input text documents in a column.')
+# scoring parameters
+DICT_TYPE = 'emfd'
+COL_NAME = "tweet_text"
+WORD_EMEDDING_MODEL = "gensim-data\\word2vec-google-news-300\\word2vec-google-news-300\\GoogleNews-vectors-negative300.bin" # path to the word embedding model
 
-    parser.add_argument('--dict_type', type=str, default='mfd',
-                        help='Dictionary for calculating FrameAxis Scores. Possible values are: emfd, mfd, mfd2')
+model = utils.setup_model(WORD_EMEDDING_MODEL)
 
-    parser.add_argument('--word_embedding_model',
-                        help='Path to the word embedding model used to map words to a vector space.')
+# paths and file names 
+OUT_CSV_PATH = "data-collection\\data\scores\\frame-axis\\{}-frame-axis.csv" # rembember to add the \{}.csv at the end
+IN_CSV_PATH = "data-collection\\data\\tweets\\{}.csv"
+FILE_NAMES = ["UKLabour-regular", "Conservatives-regular"] # names of file inside IN_CSV_PATH folder
 
-    parser.add_argument('--output_file',
-                        help='The path for saving the MF scored output CSV file.')
+# headers 
+HEADERS = ["user_id","bias_fairness","bias_care","bias_loyalty","bias_authority","bias_sanctity",
+    "intensity_fairness","intensity_care","intensity_loyalty","intensity_authority","intensity_sanctity",
+    "fairness.virtue","fairness.vice","care.virtue","care.vice","loyalty.virtue","loyalty.vice",
+    "authority.virtue","authority.vice","sanctity.virtue","sanctity.vice" ]
 
-    parser.add_argument('--docs_colname',
-                        help='The column containing the text docs to score with moral foundations.')
-    args = parser.parse_args()
-    return args
-
-
-if __name__ == '__main__':
+def calculate_score(data, output_path):
     print("Running FrameAxis Moral Foundations scores")
-    args = parse_arguments()
 
-    IN_PATH = args.input_file
-    DICT_TYPE = args.dict_type
     if DICT_TYPE not in ["emfd", "mfd", "mfd2", "customized"]:
         raise ValueError(
             f'Invalid dictionary type received: {DICT_TYPE}, dict_type must be one of \"emfd\", \"mfd\", \"mfd2\", \"customized\"')
-    OUT_CSV_PATH = args.output_file
-    DOCS_COL = args.docs_colname
-    if args.word_embedding_model is not None:
-        W2V_PATH = args.word_embedding_model
-        model = KeyedVectors.load_word2vec_format(W2V_PATH, binary=False)
-    else:
-        print('Downloading word embedding model: word2vec-google-news-300')
-        import gensim.downloader
-
-        model = gensim.downloader.load('word2vec-google-news-300')
-
-    data = pd.read_csv(IN_PATH, lineterminator='\n')
 
     fa = FrameAxis(mfd=DICT_TYPE, w2v_model=model)
-    mf_scores = fa.get_fa_scores(df=data, doc_colname=DOCS_COL, tfidf=False, format="virtue_vice",
-                                 save_path=OUT_CSV_PATH)
+
+    with lock:
+        mf_scores = fa.get_fa_scores(df=data, doc_colname=COL_NAME, tfidf=False, format="virtue_vice",
+                        save_path=output_path)
+    
+if __name__ == "__main__":
+    df_chunks = {}
+
+    # read every file
+    for name in FILE_NAMES:
+        print(f"Reading {name}")
+
+        # set paths
+        input_path = IN_CSV_PATH.format(name)
+        output_path = OUT_CSV_PATH.format(name)
+     
+        # read csv
+        df = pd.read_csv(input_path, on_bad_lines='skip', encoding='utf-8')
+        df = df.astype("string")
+
+        # split data
+        df_chunks[name] = [df[i:i+CHUNK_SIZE].reset_index() for i in range(0, len(df), CHUNK_SIZE)]
+
+        # create NUMBER_OF_PROCESSES subchunks
+        chunks = df_chunks[name]
+        subchunks = [chunks[i:i+NUMBER_OF_PROCESSES] for i in range(0, len(chunks), NUMBER_OF_PROCESSES)]
+
+        # write file header
+        output_file = open(output_path, "a", encoding="utf-8")
+        utils.write_file(output_file, *HEADERS)
+        output_file.close()
+
+        # run processes
+        for chunks in subchunks:
+            processes = [mp.Process(target=calculate_score, args=(chunk, output_path)) for chunk in chunks]
+
+            for p in processes:
+                p.start()
+
+            for p in processes:
+                p.join()
+
+    
