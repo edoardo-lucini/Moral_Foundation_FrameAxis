2497a42d2fa810a0d88d94aed2c5e8d95253a775
edoardo-lucini <edo.lucio53@gmail.com>
2023-09-12 09:41:22 +0200

    update

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import pandas as pd
 import multiprocessing as mp
 import os
 import utils
+from aggregate_account_scores import aggregate_accounts_scores
 
-from frameAxis import FrameAxis 
+from frameAxis import FrameAxis
 
 lock = mp.Lock()
 dirname = os.path.dirname(__file__)
@@ -15,20 +16,37 @@ CHUNK_SIZE = 100000
 # scoring parameters
 DICT_TYPE = 'emfd'
 COL_NAME = "tweet_text"
-WORD_EMEDDING_MODEL = "gensim-data\\word2vec-google-news-300\\word2vec-google-news-300\\GoogleNews-vectors-negative300.bin" # path to the word embedding model
+# path to the word embedding model
+WORD_EMEDDING_MODEL = "gensim-data\\word2vec-google-news-300\\word2vec-google-news-300\\GoogleNews-vectors-negative300.bin"
 
+# setup the model
 model = utils.setup_model(WORD_EMEDDING_MODEL)
 
-# paths and file names 
-OUT_CSV_PATH = "data\\scores\\frame-axis\\{}-frame-axis.csv" # rembember to add the \{}.csv at the end
+# paths and file names
+# rembember to add the \{}.csv at the end
+OUT_CSV_PATH = "data\\scores\\frame-axis\\raw\\{}-frame-axis.csv"
 IN_CSV_PATH = "data\\tweets\\{}.csv"
-FILE_NAMES = ["users-regular"] # names of file inside IN_CSV_PATH folder
 
-# headers 
-HEADERS = ["user_id","bias_fairness","bias_care","bias_loyalty","bias_authority","bias_sanctity",
-    "intensity_fairness","intensity_care","intensity_loyalty","intensity_authority","intensity_sanctity",
-    "fairness.virtue","fairness.vice","care.virtue","care.vice","loyalty.virtue","loyalty.vice",
-    "authority.virtue","authority.vice","sanctity.virtue","sanctity.vice" ]
+# names of file inside IN_CSV_PATH folder
+FILE_NAMES = ["right-regular{}", "left-regular{}"]
+
+# headers
+HEADERS = ["user_id", "bias_fairness", "bias_care", "bias_loyalty", "bias_authority", "bias_sanctity",
+           "intensity_fairness", "intensity_care", "intensity_loyalty", "intensity_authority", "intensity_sanctity",
+           "fairness.virtue", "fairness.vice", "care.virtue", "care.vice", "loyalty.virtue", "loyalty.vice",
+           "authority.virtue", "authority.vice", "sanctity.virtue", "sanctity.vice"]
+
+AGGREGATED_OUTPUT_PATH = "data\\scores\\frame-axis\\aggregated"
+
+KEYWORD = "#climatechange"
+
+
+def filter_tweets(tweets_df, keyword):
+    filtered_df = tweets_df[tweets_df["tweet_text"].str.contains(
+        keyword, case=False)]
+
+    return filtered_df
+
 
 def calculate_score(data, output_path):
     print("Running FrameAxis Moral Foundations scores")
@@ -41,9 +59,10 @@ def calculate_score(data, output_path):
 
     with lock:
         mf_scores = fa.get_fa_scores(df=data, doc_colname=COL_NAME, tfidf=False, format="virtue_vice",
-                        save_path=output_path)
-    
-if __name__ == "__main__":
+                                     save_path=output_path)
+
+
+def main():
     df_chunks = {}
 
     # read every file
@@ -51,19 +70,22 @@ if __name__ == "__main__":
         print(f"Reading {name}")
 
         # set paths
-        input_path = IN_CSV_PATH.format(name)
-        output_path = OUT_CSV_PATH.format(name)
-     
+        input_path = IN_CSV_PATH.format(name.format(""))
+        output_path = OUT_CSV_PATH.format(name.format(KEYWORD))
+
         # read csv
         df = pd.read_csv(input_path, on_bad_lines='skip', encoding='utf-8')
         df = df.astype("string")
+        df = filter_tweets(df, KEYWORD)
 
         # split data
-        df_chunks[name] = [df[i:i+CHUNK_SIZE].reset_index() for i in range(0, len(df), CHUNK_SIZE)]
+        df_chunks[name] = [df[i:i+CHUNK_SIZE].reset_index()
+                           for i in range(0, len(df), CHUNK_SIZE)]
 
         # create NUMBER_OF_PROCESSES subchunks
         chunks = df_chunks[name]
-        subchunks = [chunks[i:i+NUMBER_OF_PROCESSES] for i in range(0, len(chunks), NUMBER_OF_PROCESSES)]
+        subchunks = [chunks[i:i+NUMBER_OF_PROCESSES]
+                     for i in range(0, len(chunks), NUMBER_OF_PROCESSES)]
 
         # write file header
         output_file = open(output_path, "a", encoding="utf-8")
@@ -72,7 +94,8 @@ if __name__ == "__main__":
 
         # run processes
         for chunks in subchunks:
-            processes = [mp.Process(target=calculate_score, args=(chunk, output_path)) for chunk in chunks]
+            processes = [mp.Process(target=calculate_score, args=(
+                chunk, output_path)) for chunk in chunks]
 
             for p in processes:
                 p.start()
@@ -80,4 +103,17 @@ if __name__ == "__main__":
             for p in processes:
                 p.join()
 
-    
+        print("Aggregating")
+
+        # aggregate users scores means
+        file_to_aggregate_path = OUT_CSV_PATH.format(name.format(KEYWORD))
+
+        to_agg_df = pd.read_csv(file_to_aggregate_path)
+        print(to_agg_df.head())
+
+        aggregated_file_path = f"{AGGREGATED_OUTPUT_PATH}\\{name.format(KEYWORD)}-aggregated.csv"
+        aggregate_accounts_scores(file_to_aggregate_path, aggregated_file_path)
+
+
+if __name__ == "__main__":
+    main()
